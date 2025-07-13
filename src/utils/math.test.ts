b9dc421240dcf954b7f4cb889031fb66f08a94a8
Ryan Goree <goree.ryan@gmail.com>
2025-07-13 17:45:51 -0500

    Improve math evaluator

--- src/utils/math.test.ts
+++ src/utils/math.test.ts
@@ -1,40 +1,113 @@
-import { evalExpression } from "src/utils/math";
-import { assert, describe, expect, test } from "vitest";
+import { evaluate } from "src/utils/math";
+import { assert, describe, expect, it } from "vitest";
 
-describe("evalCalcString", () => {
-  test("should evaluate valid calculations", { timeout: 2000 }, () => {
-    expect(evalExpression("1+2")).toBe(String(1 + 2));
-    expect(evalExpression("2-3")).toBe(String(2 - 3));
-    expect(evalExpression("4*5")).toBe(String(4 * 5));
-    expect(evalExpression("8/4")).toBe(String(8 / 4));
-    expect(evalExpression("10+5-2")).toBe(String(10 + 5 - 2));
-    expect(evalExpression("100-50/2")).toBe(String(100 - 50 / 2));
-    expect(evalExpression("3*2+1")).toBe(String(3 * 2 + 1));
-    expect(evalExpression("-1*2*3*4-5")).toBe(String(-1 * 2 * 3 * 4 - 5));
-    expect(evalExpression("10*5/2*10")).toBe(String(((10 * 5) / 2) * 10));
-    expect(evalExpression("10+-5")).toBe(String(10 + -5));
-    expect(evalExpression("10*-5")).toBe(String(10 * -5));
+describe("evaluate", () => {
+  it("evaluates single numbers", () => {
+    expect(evaluate("1")).toBe(1);
+    expect(evaluate("12")).toBe(12);
+    expect(evaluate("12.34")).toBe(12.34);
+    expect(evaluate(".12")).toBe(0.12);
+    expect(evaluate("12.")).toBe(12);
+    expect(evaluate("1_2")).toBe(1_2);
   });
 
-  test("should throw error for invalid calculations", () => {
-    const invalidCalcs = [
-      "1+",
-      "1+x",
-      "1++1",
-      "2**3",
-      "4--2",
-      "8//4",
-      "10+5**2",
-      "100-50//2",
+  it("evaluates basic expressions", { timeout: 2000 }, () => {
+    expect(evaluate("1 + 2")).toBe(1 + 2);
+    expect(evaluate("2 - 3")).toBe(2 - 3);
+    expect(evaluate("4 * 5")).toBe(4 * 5);
+    expect(evaluate("8 / 4")).toBe(8 / 4);
+    expect(evaluate("2 ** 2")).toBe(2 ** 2);
+    expect(evaluate("10 % 6")).toBe(10 % 6);
+  });
+
+  it("evaluates multi-step expressions", { timeout: 2000 }, () => {
+    expect(evaluate("1 + 2 - 3")).toBe(1 + 2 - 3);
+    expect(evaluate("2 - 3 + 4")).toBe(2 - 3 + 4);
+    expect(evaluate("4 * 5 / 10")).toBe((4 * 5) / 10);
+    expect(evaluate("8 / 4 * 6")).toBe((8 / 4) * 6);
+    expect(evaluate("2 ** 3 ** 2")).toBe(2 ** (3 ** 2));
+  });
+
+  it("respects order of operations", { timeout: 2000 }, () => {
+    expect(evaluate("1 + 2 * 3 - 4")).toBe(1 + 2 * 3 - 4);
+    expect(evaluate("2 - 3 / 4 + 5")).toBe(2 - 3 / 4 + 5);
+  });
+
+  it("handles division by zero", { timeout: 2000 }, () => {
+    expect(evaluate("1 / 0")).toBe(1 / 0);
+    expect(evaluate("1 / 0 / 2")).toBe(1 / 0 / 2);
+  });
+
+  it("evaluates unary operators", () => {
+    expect(evaluate("+12")).toBe(+12);
+    expect(evaluate("+12")).toBe(+12);
+    expect(evaluate("-12")).toBe(-12);
+    expect(evaluate("+-12")).toBe(+-12);
+    expect(evaluate("-+12")).toBe(-+12);
+    expect(evaluate("+-+-+12")).toBe(+-+-+12);
+    expect(evaluate("-+-+-12")).toBe(-+-+-12);
+  });
+
+  it("evaluates parentheses", () => {
+    expect(evaluate("10 - (5 + 2)")).toBe(10 - (5 + 2));
+    expect(evaluate("(1 + 2) * 3")).toBe((1 + 2) * 3);
+    expect(evaluate("4 / (2 - 1)")).toBe(4 / (2 - 1));
+    expect(evaluate("4 / ((2 - 1))")).toBe(4 / (2 - 1));
+  });
+
+  it("evaluates scientific expression", () => {
+    expect(evaluate("1e2")).toBe(1e2);
+    expect(evaluate("1E2")).toBe(1e2);
+    expect(evaluate("12e3")).toBe(12e3);
+    expect(evaluate("12.34e5")).toBe(12.34e5);
+    expect(evaluate("12.34e-2")).toBe(12.34e-2);
+  });
+
+  it("evaluates complex expressions", () => {
+    expect(evaluate("2e2 * -+3 / 6 + 3 ** (3 - 1) - 1")).toBe(
+      (2e2 * -+3) / 6 + 3 ** (3 - 1) - 1,
+    );
+  });
+
+  it("evaluated bitwise operators", () => {
+    expect(evaluate("9 | 26")).toBe(9 | 26);
+    expect(evaluate("9 ^ 26")).toBe(9 ^ 26);
+    expect(evaluate("9 & 26")).toBe(9 & 26);
+    expect(evaluate("26 >> 2")).toBe(26 >> 2);
+    expect(evaluate("26 << 2")).toBe(26 << 2);
+  });
+
+  it("throws error for invalid expressions", () => {
+    const invalidExpressions = [
+      "++12",
+      "--12",
+      "*12",
+      "/12",
+      "12+",
+      "12-",
+      "12*",
+      "12/",
+      "12++34",
+      "12--34",
+      "12***34",
+      "12x",
+      "12e1.2",
+      "12||34",
+      "12^^34",
+      "12&&34",
+      "12>>>34",
+      "12<<<34",
+      "1+(2*3",
     ];
-    for (const calc of invalidCalcs) {
+    for (const expression of invalidExpressions) {
       let error: unknown;
       try {
-        evalExpression(calc);
+        error = evaluate(expression);
       } catch (e) {
+        // console.error(e);
         error = e;
       }
-      assert(error instanceof Error, `Expected error for "${calc}"`);
+      assert(error instanceof Error, `Expected error for "${expression}"`);
     }
   });
 });
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,36 +1,174 @@
-export const operators = ["+", "-", "*", "/"];
+// A quick and dirty math expression evaluator. I tried a few libraries, but
+// they were either too heavy or didn't support syntax supported by Mathler. I
+// suspect Mathler is using `eval` or `new Function` under the hood, but I
+// wanted to see if I could support the same syntax without arbitrary code
+// execution. A multi-step lexer + parser would be more robust, but this was a
+// fun exercise to see how far I could get with regex and string manipulation.
 
 /**
- * Evaluate a simple mathematical expression string made up of integers and the
- * four basic arithmetic {@linkcode operators}.
+ * Concatenates a sequence of strings and regular expressions into a single
+ * regular expression.
  */
-export function evalExpression(calc: string) {
-  calc = calc.replaceAll(" ", "");
+function concatPatterns(patterns: (string | RegExp)[], flags?: string): RegExp {
+  return new RegExp(
+    patterns.map((p) => (p instanceof RegExp ? p.source : p)).join(""),
+    flags,
+  );
+}
 
-  // Validate format
-  // https://regex101.com/r/O3Gxgo/6
-  if (!/^-?\d+((-|[+*/]-?)\d+)+$/.test(calc)) {
-    throw new Error(`Invalid calculation string: "${calc}"`);
+// https://regex101.com/r/VV3B0V/1
+const unaryRegex = /(?<!\d)(?:(?<!\+)\+|(?<!-)-)/;
+// https://regex101.com/r/CBRWl6/1
+const unsignedNumRegex = /\d*\.?\d*(?:e[+-]?\d+)?/i;
+// https://regex101.com/r/PhjIPJ/1
+const terminalRegex = concatPatterns(
+  ["^(", unaryRegex, ")*?", unsignedNumRegex, "$"],
+  "i",
+);
+
+/**
+ * Evaluates a terminal expression (a single number preceded by optional
+ * unary operators).
+ */
+function evaluateTerminal(expression: string) {
+  if (!expression.match(terminalRegex)) {
+    throw new Error(`Invalid expression: ${expression}`);
   }
-
-  // Multiply and divide
-  while (calc.includes("*") || calc.includes("/")) {
-    // https://regex101.com/r/yQeJ20/1
-    calc = calc.replace(/((?:^-)?\d+)([*/])(-?\d+)/, (_, a, op, b) => {
-      const result = op === "*" ? +a * +b : +a / +b;
-      return String(result);
-    });
+  const mantissa = expression.split("e")[0];
+  const negativeSigns = mantissa?.match(/-/g) || [];
+  const sign = negativeSigns.length % 2 ? "-" : "";
+  const abs = expression.replace(/^[+-]+/g, "");
+  const num = Number(`${sign}${abs}`);
+  if (Number.isNaN(num)) {
+    throw new Error(`Invalid number: ${expression}`);
   }
+  return num;
+}
 
-  // Add and subtract
-  // Slice the first char to avoid matching negative signs
-  while (calc.includes("+") || calc.slice(1).includes("-")) {
-    // https://regex101.com/r/rLQsGA/1
-    calc = calc.replace(/((?:^-)?\d+)([+-])(-?\d+)/, (_, a, op, b) => {
-      const result = op === "+" ? +a + +b : +a - +b;
-      return String(result);
-    });
+/**
+ * A list of operators with their evaluation functions, ordered by precedence.
+ */
+const operatorPrecedence: {
+  operator: string;
+  evaluate: (left: number, right: number) => number;
+  associativity?: "left" | "right";
+}[] = [
+  {
+    operator: "**",
+    evaluate: (left, right) => left ** right,
+    associativity: "right",
+  },
+  {
+    operator: "/",
+    evaluate: (left, right) => left / right,
+  },
+  {
+    operator: "*",
+    evaluate: (left, right) => left * right,
+  },
+  {
+    operator: "%",
+    evaluate: (left, right) => left % right,
+  },
+  {
+    operator: "-",
+    evaluate: (left, right) => left - right,
+  },
+  {
+    operator: "+",
+    evaluate: (left, right) => left + right,
+  },
+  {
+    operator: "<<",
+    evaluate: (left, right) => left << right,
+  },
+  {
+    operator: ">>",
+    evaluate: (left, right) => left >> right,
+  },
+  {
+    operator: "&",
+    evaluate: (left, right) => left & right,
+  },
+  {
+    operator: "^",
+    evaluate: (left, right) => left ^ right,
+  },
+  {
+    operator: "|",
+    evaluate: (left, right) => left | right,
+  },
+];
+
+/**
+ * Creates an operator evaluator function that can evaluate expressions with
+ * the given operator.
+ */
+function createOperatorEvaluator({
+  operator,
+  evaluateOperand,
+  evaluate,
+  associativity = "left",
+}: {
+  operator: string;
+  evaluateOperand: (expression: string) => number;
+  evaluate: (left: number, right: number) => number;
+  associativity?: "left" | "right";
+}) {
+  // https://regex101.com/r/WrhG2f/1
+  const operatorRegex = concatPatterns([
+    "(?<=\\d)",
+    operator.replace(/([+*|^&])/g, "\\$1"),
+    "(?=",
+    unaryRegex,
+    "*[\\d.])",
+  ]);
+  return (expression: string): number => {
+    if (!expression.includes(operator)) {
+      return evaluateOperand(expression);
+    }
+    const operands = expression.split(operatorRegex);
+    const isLeftAssociative = associativity === "left";
+    const initial = isLeftAssociative ? operands.shift() : operands.pop();
+    if (!initial) return 0;
+    const initialValue = evaluateOperand(initial);
+    if (!operands.length) return initialValue;
+    return isLeftAssociative
+      ? operands.reduce(
+          (result, operand) => evaluate(result, evaluateOperand(operand)),
+          initialValue,
+        )
+      : operands.reduceRight(
+          (result, operand) => evaluate(evaluateOperand(operand), result),
+          initialValue,
+        );
+  };
+}
+
+let evaluateOperand = evaluateTerminal;
+for (const { operator, evaluate, associativity } of operatorPrecedence) {
+  evaluateOperand = createOperatorEvaluator({
+    operator,
+    evaluateOperand,
+    evaluate,
+    associativity,
+  });
+}
+
+const removableCharsRegex = /_|\s+|\/\/.*$/g;
+const parenthesesRegex = /\(([^()]+)\)/g;
+
+/**
+ * Evaluates a mathematical expression string and returns the result. Supports
+ * basic arithmetic, unary operators, parentheses, scientific notation, and
+ * operator precedence.
+ */
+export function evaluate(expression: string): number {
+  expression = expression.replace(removableCharsRegex, "");
+  while (parenthesesRegex.test(expression)) {
+    expression = expression.replace(parenthesesRegex, (_, inner) =>
+      String(evaluateOperand(inner)),
+    );
   }
-
-  return calc;
+  return evaluateOperand(expression);
 }
